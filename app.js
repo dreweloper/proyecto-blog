24e588650847374b9ab9f49709f89d72cee0aceb
DREWiex <aleon88@gmail.com>
2023-04-02 00:21:27 +0200

    Configuro carpeta estática y creo carpetas y archivos css y js

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const port = process.env.PORT;
 
 app.use(cors());
 
+// static folder
+app.use(express.static(`${__dirname}/public`));
+
+
+// routes
+app.use((req, res, next) => {
+    res.status(404).render('404', {
+        error: '404',
+        msg: 'Página no encontrada'
+    })
+});
 
 
 app.listen(port, () => console.log(`Conectado al puerto: ${port}`));
