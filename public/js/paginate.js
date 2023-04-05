753f6c1c991a5eb5beb8328d39e6f36f5845b886
DREWiex <aleon88@gmail.com>
2023-04-05 13:31:10 +0200

    En principio, función de pintar botones de paginación terminada. Pendiente prueba

--- public/js/paginate.js
+++ public/js/paginate.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     //*** VARIABLES ***//
-    const div = document.querySelector('#paginate');
+    const divPagination = document.querySelector('#paginate');
     // const prevBtn = document.querySelector('#btn-prev');
     // const nextBtn = document.querySelector('#btn-next');
+    const page = 1;
 
 
 
@@ -11,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('click', ({target}) => {
 
         if(target.matches('#btn-prev')){
-            console.log('Botón prev')
+
         };
 
         if(target.matches('#btn-next')){
@@ -24,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //*** FUNCIONES ***//
 
-    const fetchingData = async (page = 1, limit = 3) => {
+    const fetchingData = async (page = 1, limit = 3) => { //? pendiente quitar los valores por defecto
 
         const url = `http://localhost:3000/api/?page=${page}&limit=${limit}`
 
@@ -52,12 +53,39 @@ document.addEventListener('DOMContentLoaded', () => {
     }; //!FUNC-FETCHINGDATA
 
 
+    const btnsPagination = async (page, prevPage, nextPage) => {
+
+        divPagination.innerHTML = '';
+
+        if(prevPage != null){
+            const btnPrev = document.createElement('BUTTON');
+            btnPrev.id = 'btn-prev';
+            btnPrev.textContent = 'PREV BTN';
+            divPagination.append(btnPrev);
+        };
+
+        const btnPage = document.createElement('BUTTON');
+        btnPage.textContent = page;
+        divPagination.append(btnPage);
+
+        if(nextPage != null){
+            const btnNext = document.createElement('BUTTON');
+            btnNext.id = 'btn-next';
+            btnNext.textContent = 'NEXT BTN'
+            console.log('Pintar botón next');
+        } else {
+            divPagination.append(btnNext);
+        };
+
+    }; //!FUNC-BTNSPAGINATION
+
+
     const init = async () => {
 
         await fetchingData();
 
-    };
+    }; //!FUNC-INIT
 
-    //init();
+    init();
 
 }); //!LOAD
