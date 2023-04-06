f1c2ddd913c1e0740694f884e55096d91baed2b2
DREWiex <aleon88@gmail.com>
2023-04-06 14:21:58 +0200

    Ya guardo el usuario con el password encriptado. Siguiente paso: token

--- controllers/controllerBackUsersAPI.js
+++ controllers/controllerBackUsersAPI.js
@@ -1,17 +1,28 @@
 const User = require('../models/modelUser');
+const bcrypt = require('bcryptjs');
 
 
 const createUser = async (req, res) => {
 
+    // encriptar password
+    let salt = bcrypt.genSaltSync(10);
+    req.body.password = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User(req.body);
 
     try {
-      
+        
         const user = await newUser.save();
+
+        // generar token
 
         return res.status(201).json({
             ok: true,
-            user
+            uid: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+
         });
 
     } catch (error) {
