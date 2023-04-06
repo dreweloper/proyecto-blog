488ee352e7cd139bfcac6d31388116947b5223eb
DREWiex <aleon88@gmail.com>
2023-04-06 13:09:15 +0200

    Model user creado.

--- models/modelEntry.js
+++ models/modelEntry.js
@@ -8,26 +8,30 @@ const entrySchema = new Schema({
         type: String,
         required: true
     },
+
     extract: {
         type: String,
         required: true
     },
+
     body: {
         type: String,
         required: true
     },
+
     photo: {
         type: String, // url (http://localhost:3000/images/…)
         required: true
     },
+
     date: {
         type: Date,
         default: Date.now
-    },
+    }
 
 }, {
 
-    timestamps: true, // mongoose añade dos propiedades de tipo Date al Schema: "createdAt", fecha de creación del documento; "updateAt", fecha de la última act. del documento
+    timestamps: true // mongoose añade dos propiedades de tipo Date al Schema: "createdAt", fecha de creación del documento; "updateAt", fecha de la última act. del documento
 
 });
 
